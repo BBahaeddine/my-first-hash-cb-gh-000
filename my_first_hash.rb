9c7d5664d7462c2819b4b381d733db1d241e6139
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-09 11:19:42 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,7 +12,7 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 the_manifest = {"whale bone corsets" => 5,
   "porcelain vases" => 2,
-  
+  "oil paintings" => 3
 }
 
 end
@@ -25,6 +25,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  return shipping_manifest["oil paintings"]
 
 end
 
